03ff28f3ac6fe2182814f20063c1b540a002042f
moiseevanton <athidif94@gmail.com>
2016-05-08 15:11:33 -0400

    Final code

--- Vehicles/Vehicle.h
+++ Vehicles/Vehicle.h
@@ -10,4 +10,25 @@
 
 @interface Vehicle : NSObject
 
+// properties (characteristics)
+@property (nonatomic, assign) NSInteger numberOfWheels;
+@property (nonatomic, copy) NSString *powerSource;
+@property (nonatomic, copy) NSString *brandName;
+@property (nonatomic, copy) NSString *modelName;
+@property (nonatomic, assign) NSInteger modelYear;
+
+//Basic operation methods
+-(NSString *)goForward;
+-(NSString *)goBackward;
+-(NSString *)stopMoving;
+-(NSString *)changeGears:(NSString *)newGearName;
+-(NSString *)turn:(NSInteger)degrees;
+-(NSString *)makeNoise;
+
+//Convenience method for UITableViewCells and UINavigationBar titles.
+-(NSString *)vehicleTitleString;
+
+//Convenience method to get the vehicle's details.
+-(NSString *)vehicleDetailsString;
+
 @end
